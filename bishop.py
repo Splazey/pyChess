6b2711e074b9080357470f53e53e1ff000bcb4d3
Splazey <121494341+Splazey@users.noreply.github.com>
2024-10-29 13:50:18 +0800

    Bishop Movement

--- bishop.py
+++ bishop.py
@@ -23,4 +23,61 @@ class Bishop(Piece):
         self.img = pygame.transform.smoothscale(self.img, (100, 100))
 
     def checkLegal(self, x, y, board):
-        pass
+        
+        legal = []
+        directions = [ # the four diagonals a bishop can travel through
+            
+            [1,1],
+            [-1,-1],
+            [1,-1],
+            [-1,1]
+        
+        ]
+
+
+
+        # for d in directions:
+
+        #     x_offset = d[0]
+        #     y_offset = d[1]
+
+        #     while 0 <= x + x_offset < 8 and 0 <= y + y_offset < 8:
+        #         if isinstance(board[y + y_offset][x + x_offset], Piece):
+
+        #             print(f"Blocking Piece! in {chr(x + x_offset + 97) + str((9 - y) - y_offset - 1)}")
+        #             if board[y + y_offset][x + x_offset].white != self.white:
+        #                  legal.append(chr(x + x_offset + 97) + str((9 - y) - y_offset - 1)) # add that tile to the legal moves array
+        #             break
+        #         else:
+        #             legal.append(chr(x + x_offset + 97) + str((9 - y) - y_offset - 1)) # add that tile to the legal moves array
+
+        #     x_offset += d[0]
+        #     y_offset += d[1]
+
+
+    
+        for d in directions:
+            
+            x_offset = d[0]
+            y_offset= d[1]
+
+            if x_offset == -1 or y_offset == -1:
+                pos = False
+
+            while 0 <= y + y_offset < 8 and 0 <= x + x_offset < 8:
+                if isinstance(board[y + y_offset][x + x_offset], Piece):
+                    print(f"Blocking Piece! in {chr(x + x_offset + 97) + str((9 - y) - y_offset - 1)}")
+
+                    if board[y + y_offset][x + x_offset].white != self.white:
+                        # the blocking piece is an enemy piece
+                        print("enemy in range!")
+                        legal.append(chr(x + x_offset + 97) + str((9 - y) - y_offset - 1)) # add that tile to the legal moves array
+                    break # stop exploring further since there is a blocking piece
+                else:
+                    legal.append(chr(x + x_offset + 97) + str((9 - y) - y_offset - 1)) # add that tile to the legal moves array
+                    
+                    x_offset += d[0]
+                    y_offset += d[1]
+
+        print(f"Legal moves: {legal}")
+        return legal
